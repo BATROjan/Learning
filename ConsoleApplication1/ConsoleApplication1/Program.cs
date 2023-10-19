f161f9ee5ce2afa0624e2fb14c993f1a0e1142bd
Batyzhan <>
2023-10-19 11:54:26 +0700

    even/odd numder

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("введите первое число");
+      int firstNumber = int.Parse(Console.ReadLine());
+      Console.WriteLine("введите первое число");
+      int secondNumber = int.Parse(Console.ReadLine());
+      int currentNumber = firstNumber;
+      int CountOfEvenNumber = 0;
+      int CountOfOddNumber = 0;
+      while ( currentNumber <=secondNumber)
+      {
+        if (currentNumber%2==0)
+        {
+          CountOfEvenNumber++;
+        }
+        else
+        {
+          CountOfOddNumber++;
+        }
+        currentNumber++;
+      }
+      Console.WriteLine("в диапазоне от " + firstNumber + " до " + secondNumber);
+      Console.WriteLine("кол-во четных чисел " + CountOfEvenNumber);
+      Console.WriteLine("кол-во нечетных чисел " + CountOfOddNumber);
+    }
+  }
+}
